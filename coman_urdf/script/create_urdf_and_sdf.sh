d6de27a373744a91929221fbbd17aaae091f4902
Alessio Rocchi <Alessio.Rocchi@iit.it>
2013-11-06 19:37:00 +0100

    Fixed create_urdf_and_sdf to export middleware environment variables after setting them and before running the ros xacro script, and unsetting them after the xacro and gazebowtf scripts terminate.

--- coman_urdf/script/create_urdf_and_sdf.sh
+++ coman_urdf/script/create_urdf_and_sdf.sh
@@ -8,30 +8,36 @@ echo "...urdf correctly created!"
 
 if [ $# == "ros" ]
   then
-    GAZEBO_COMAN_USES_ROS=1
-    GAZEBO_COMAN_USES_YARP=0
+    GAZEBO_COMAN_USES_ROS=true
+    GAZEBO_COMAN_USES_YARP=false
     echo "Creating urdf of coman_robot.urdf.xacro with ROS plugins"
    else
-    GAZEBO_COMAN_USES_ROS=0
-    GAZEBO_COMAN_USES_YARP=1
+    GAZEBO_COMAN_USES_ROS=false
+    GAZEBO_COMAN_USES_YARP=true
     echo "Creating urdf of coman_robot.urdf.xacro with YARP plugins"
 fi
+
+export GAZEBO_COMAN_USES_YARP
+export GAZEBO_COMAN_USES_ROS
 rosrun xacro xacro.py coman_robot.urdf.xacro > coman_robot.urdf
 echo "...urdf correctly created!"
 
 echo "Creating sdf of coman_robot.urdf..."
 gzsdf print coman_robot.urdf > coman.sdf
 
-if [ $GAZEBO_COMAN_USES_YARP -eq 1 ]
+if [ $GAZEBO_COMAN_USES_YARP ]
   then
     python ../script/gazebowtf.py coman.gazebo.wtf > coman2.sdf
     mv coman2.sdf coman.sdf
 fi
 
+unset GAZEBO_COMAN_USES_YARP
+unset GAZEBO_COMAN_USES_ROS
+
 echo "...sdf correctly created!"
 
 echo "Removing coman_robot.urdf."
-rm coman_robot.urdf
+#rm coman_robot.urdf
 
 echo "Copying coman.sdf in coman_gazebo/sdf."
 cp coman.sdf ../../coman_gazebo/sdf
