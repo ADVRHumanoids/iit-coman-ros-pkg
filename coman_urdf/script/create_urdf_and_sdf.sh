53d126b33697602c5b773178557058a276866047
Alessio Rocchi <alessio.rocchi@iit.it>
2014-01-31 17:02:47 +0100

    Added new feet. Added flags to compile coman with and without forearms, with and without hands, with old feet and with new feet

--- coman_urdf/script/create_urdf_and_sdf.sh
+++ coman_urdf/script/create_urdf_and_sdf.sh
@@ -6,37 +6,53 @@ echo "Creating bare urdf of coman.urdf.xacro"
 rosrun xacro xacro.py coman.urdf.xacro > coman.urdf
 echo "...urdf correctly created!"
 
+# TODO checks (must be mutually exclusive)
+export GAZEBO_COMAN_USES_SIMPLE_FEET_DRC_sw1_14=true
+export GAZEBO_COMAN_USES_ROUND_FEET_13=false
+
+# TODO: checks (must be mutually exclusive).. eventually, a menu!... documentation!
+#       at the moment just the only combinations that work are:
+# 
+#       1) NO_FOREARMS + NO_HANDS
+#
+#export GAZEBO_COMAN_USES_NO_FOREARMS=true
+#export GAZEBO_COMAN_USES_NO_HANDS=true
+#export GAZEBO_COMAN_USES_7DOF_FOREARMS_DRC_14=false
+#export GAZEBO_COMAN_USES_SOFTHANDS_DRC_sw1_14=false
+#
+#       2) 7DOFS_FOREARMS + SOFTHANDS
+export GAZEBO_COMAN_USES_NO_FOREARMS=false
+export GAZEBO_COMAN_USES_NO_HANDS=false
+export GAZEBO_COMAN_USES_7DOF_FOREARMS_DRC_14=true
+export GAZEBO_COMAN_USES_SOFTHANDS_DRC_sw1_14=true
+
 if [ $# -gt 0 ]
   then
   if [ $1 == "ros" ] 
     then
-      GAZEBO_COMAN_USES_ROS=true
-      GAZEBO_COMAN_USES_YARP=false
-      GAZEBO_COMAN_USES_ACCURATE_FT=false
+      export GAZEBO_COMAN_USES_ROS=true
+      export GAZEBO_COMAN_USES_YARP=false
+      export GAZEBO_COMAN_USES_ACCURATE_FT=false
       echo "Creating urdf of coman_robot.urdf.xacro with ROS plugins"
   elif [ $1 == "ros_accurate_ft" ] 
     then
-      GAZEBO_COMAN_USES_ROS=true
-      GAZEBO_COMAN_USES_YARP=false
-      GAZEBO_COMAN_USES_ACCURATE_FT=true
+      export GAZEBO_COMAN_USES_ROS=true
+      export GAZEBO_COMAN_USES_YARP=false
+      export GAZEBO_COMAN_USES_ACCURATE_FT=true
       echo "Creating urdf of coman_robot.urdf.xacro with ROS plugins using accurate ft sensor description"
   elif [ $1 == "yarp_accurate_ft" ] 
     then
-      GAZEBO_COMAN_USES_ROS=false
-      GAZEBO_COMAN_USES_YARP=true
-      GAZEBO_COMAN_USES_ACCURATE_FT=true
+      export GAZEBO_COMAN_USES_ROS=false
+      export GAZEBO_COMAN_USES_YARP=true
+      export GAZEBO_COMAN_USES_ACCURATE_FT=true
       echo "Creating urdf of coman_robot.urdf.xacro with YARP plugins using accurate ft sensor description"
   fi
 else
-    GAZEBO_COMAN_USES_ROS=false
-    GAZEBO_COMAN_USES_YARP=true
-    GAZEBO_COMAN_USES_ACCURATE_FT=false
+    export GAZEBO_COMAN_USES_ROS=false
+    export GAZEBO_COMAN_USES_YARP=true
+    export GAZEBO_COMAN_USES_ACCURATE_FT=false
     echo "Creating urdf of coman_robot.urdf.xacro with YARP plugins"
 fi
-
-export GAZEBO_COMAN_USES_YARP
-export GAZEBO_COMAN_USES_ROS
-export GAZEBO_COMAN_USES_ACCURATE_FT
 
 rosrun xacro xacro.py coman_robot.urdf.xacro > coman_robot.urdf
 echo "...urdf correctly created!"
@@ -51,6 +67,12 @@ mv coman2.sdf coman.sdf
 unset GAZEBO_COMAN_USES_YARP
 unset GAZEBO_COMAN_USES_ROS
 unset GAZEBO_COMAN_USES_ACCURATE_FT
+unset GAZEBO_COMAN_USES_ROUND_FEET_13
+unset GAZEBO_COMAN_USES_SIMPLE_FEET_DRC_sw1_14
+unset GAZEBO_COMAN_USES_NO_FOREARMS
+unset GAZEBO_COMAN_USES_7DOF_FOREARMS_DRC_14
+unset GAZEBO_COMAN_USES_NO_HANDS
+unset GAZEBO_COMAN_USES_SOFTHANDS_DRC_sw1_14
 
 echo "...sdf correctly created!"
 
@@ -79,6 +101,7 @@ cp -r * ../coman_urdf
 
 cd ../
 
+rm -rf ~/.gazebo/models/coman_urdf
 cp -r coman_urdf ~/.gazebo/models
 
 rm -rf coman_urdf
