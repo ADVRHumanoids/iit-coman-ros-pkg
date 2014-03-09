7127e06805fdb9bfa5a15829f88216e46e071fbc
Alessio Rocchi <alessio.rocchi@iit.it>
2014-03-09 20:37:27 +0100

    Added simple way to compile all Coman models at the same time.
    When compiling models using create_urdf_srdf_sdf.sh --all they all get copied
    inside the coman_gazebo/database folder. All possible configurations are automatically
    parsed from the urdf/conf folder.
    The file urdf/conf/coman.urdf.xacro is the default file that is copied in the coman_urdf/urdf
    folder as coman_config.urdf.xacro when compiling.
    The coman_gazebo/database folder is a gazebo models folder, so by setting GAZEBO_MODEL_PATH
    to point that folder, they will be automatically shown in gazebo.
    The default installation by CMakeLists now does not copy files in ~/.gazebo anymore,
    but the create_urdf_srdf_sdf.sh still does, for compatility reasons.
    The files will not get copied inside ~/.gazebo/models/coman_urdf but inside
    ~/.gazebo/models/coman

--- coman_urdf/script/create_urdf_srdf_sdf.sh
+++ coman_urdf/script/create_urdf_srdf_sdf.sh
@@ -1,68 +1,153 @@
 #!/bin/bash
 
+# this way the script can be called from any directory
+SCRIPT_ROOT=$(dirname $(readlink --canonicalize --no-newline $BASH_SOURCE))
+cd $SCRIPT_ROOT
 cd ../urdf
 
-echo "Creating bare urdf of coman.urdf.xacro"
-rosrun xacro xacro.py coman.urdf.xacro > coman.urdf
-echo "...urdf correctly created!"
+if [ $# -gt 0 ] ; then
+    if [ "$1" = '--all' ]; then
+        if [ -d config ]; then
 
-echo "Creating urdf of coman_robot.urdf.xacro"
-rosrun xacro xacro.py coman_robot.urdf.xacro > coman_robot.urdf
-echo "...urdf correctly created!"
+            rm -rf ~/.gazebo/models/coman_urdf
 
-echo "Creating sdf of coman_robot.urdf..."
-gzsdf print coman_robot.urdf > coman.sdf
+            echo "Regenerating database.config for coman_gazebo"
 
-python ../script/gazebowtf.py coman.gazebo.wtf coman_config.urdf.xacro > coman2.sdf
+cat > ../../coman_gazebo/database/database.config << EOF
+<?xml version='1.0'?>
+<database>
+<name>Coman Gazebo Database</name>
+<license>Creative Commons Attribution 3.0 Unported</license>
+<models>
+EOF
 
-mv coman2.sdf coman.sdf
+            for i in config/*.urdf.xacro; do
+                cd $SCRIPT_ROOT
+                cd ../urdf
+                if [ -r $i ]; then
 
-#unset GAZEBO_COMAN_USES_YARP
-#unset GAZEBO_COMAN_USES_ROS
-#unset GAZEBO_COMAN_USES_ACCURATE_FT
-#unset GAZEBO_COMAN_USES_ROUND_FEET_13
-#unset GAZEBO_COMAN_USES_SIMPLE_FEET_DRC_sw1_14
-#unset GAZEBO_COMAN_USES_NO_FOREARMS
-#unset GAZEBO_COMAN_USES_7DOF_FOREARMS_DRC_14
-#unset GAZEBO_COMAN_USES_NO_HANDS
-#unset GAZEBO_COMAN_USES_SOFTHANDS_DRC_sw1_14
+                    echo "Processing file $i"
+                    model_name="`python ../script/get_model_params.py ${i} model_name`"
+                    model_version="`python ../script/get_model_params.py ${i} model_version`"
+                    model_filename=$(basename -s ".urdf.xacro" $i)
+                    echo "${model_filename} configures model ${model_name}, version ${model_version}"
 
-echo "...sdf correctly created!"
+                    cp $i coman_config.urdf.xacro
+                    mkdir -p ../../coman_gazebo/database/$model_filename
+                    echo "<uri>file://${model_filename}</uri>" >> ../../coman_gazebo/database/database.config
 
-echo "Removing coman_robot.urdf."
-rm coman_robot.urdf
+rm -f ../../coman_gazebo/database/$model_filename/model.config                    
+cat >> ../../coman_gazebo/database/$model_filename/model.config << EOF
+<?xml version="1.0"?>
+<model>
+  <name>$model_name</name>
+  <version>$model_version</version>
+  <sdf version='1.4'>coman.sdf</sdf>
 
-echo "Copying coman.sdf in coman_gazebo/sdf."
-cp coman.sdf ../../coman_gazebo/sdf
+  <author>
+   <name>Enrico Mingo</name>
+   <email>enrico.mingo@iit.it</email>
+  </author>
 
-echo "Removing coman.sdf"
-rm coman.sdf
+  <description>
+    Simulation of the COMAN Humanoid Robot from IIT.
+  </description>
+</model>
+EOF
+                    
+                    cp ../../coman_gazebo/database/$model_filename/model.config ../../coman_gazebo/database/$model_filename/manifest.xml
+                    cp ../../coman_gazebo/database/$model_filename/model.config ../../coman_gazebo/database/$model_filename/${model_filename}.config
 
-cd ..
 
-echo "Copying meshes in coman_gazebo/sdf."
-cp -r meshes/ ../coman_gazebo/sdf
+                    echo "Creating bare urdf of coman.urdf.xacro ..."
+                    rosrun xacro xacro.py coman.urdf.xacro > ${model_filename}.urdf
+                    echo "...${model_filename}.urdf correctly created!"
 
-cd ../coman_gazebo
+                    echo "Creating sdf of coman_robot.urdf.xacro"
+                    rosrun xacro xacro.py coman_robot.urdf.xacro > coman_robot.urdf
+                    gzsdf print coman_robot.urdf > coman.sdf
+                    python ../script/gazebowtf.py coman.gazebo.wtf coman_config.urdf.xacro > coman2.sdf
+                    mv coman2.sdf coman.sdf
+                    rm coman_robot.urdf
+                    echo "...sdf correctly created!"
 
-echo "Copying all data in coman_gazebo/sdf in ~/.gazebo/models/coman_urdf"
-mkdir coman_urdf
+                    echo "Installing robot model in coman_gazebo/${model_filename}"
+                    mv coman.sdf ../../coman_gazebo/database/${model_filename}/
 
-cd sdf
+                    echo "Creating srdf from coman.srdf.xacro"
+                    cd ../../coman_srdf/srdf
+                    rosrun xacro xacro.py coman.srdf.xacro > ${model_filename}.srdf 
+                    echo "...created ${model_filename}.srdf!"
+                    
+                    echo 
+                    echo 
+                    echo "Complete! Enjoy ${model_name} ver ${model_version} in GAZEBO!"
+                    echo "If the model requires it, remember to check ../../coman_gazebo/${model_filename}/conf/"
+                    echo
+                    echo
+                fi
+            done
 
-cp -r * ../coman_urdf
+            cd $SCRIPT_ROOT
+            cd ../urdf
+            
+            echo "</models>" >> ../../coman_gazebo/database/database.config
+            echo "</database>" >> ../../coman_gazebo/database/database.config
+            
+            unset i
+            
+            rm coman_config.urdf.xacro
+            cp -r ../meshes/ ../../coman_gazebo/database/coman/
+        fi
+    fi
+else
 
-cd ../
+    cp config/coman.urdf.xacro coman_config.urdf.xacro
 
-rm -rf ~/.gazebo/models/coman_urdf
-cp -r coman_urdf ~/.gazebo/models
+    echo "Creating bare urdf of coman.urdf.xacro"
+    rosrun xacro xacro.py coman.urdf.xacro > coman.urdf
+    echo "...urdf correctly created!"
 
-rm -rf coman_urdf
+    echo "Creating urdf of coman_robot.urdf.xacro"
+    rosrun xacro xacro.py coman_robot.urdf.xacro > coman_robot.urdf
+    echo "...urdf correctly created!"
 
-echo "Creating srdf from coman.srdf.xacro"
-cd ../coman_srdf/srdf
-rosrun xacro xacro.py coman.srdf.xacro > coman.srdf 
-echo "...created coman.srdf!"
- 
-echo "Finish! Enjoy COMAN in GAZEBO!"
+    echo "Creating sdf of coman_robot.urdf..."
+    gzsdf print coman_robot.urdf > coman.sdf
 
+    python ../script/gazebowtf.py coman.gazebo.wtf coman_config.urdf.xacro > coman2.sdf
+
+    mv coman2.sdf coman.sdf
+
+    echo "...sdf correctly created!"
+
+    echo "Removing coman_robot.urdf."
+    rm coman_robot.urdf
+
+
+    echo "Moving coman.sdf in coman_gazebo/sdf."
+    mv coman.sdf ../../coman_gazebo/sdf/
+
+    echo "Copying meshes in coman_gazebo/sdf."
+    cp -r ../meshes/ ../../coman_gazebo/sdf/
+
+    cd ../../coman_gazebo
+
+    echo "Copying all data in coman_gazebo/sdf in ~/.gazebo/models/coman_urdf"
+
+    rm -rf ~/.gazebo/models/coman_urdf
+    rm -rf ~/.gazebo/models/coman
+    cp -r sdf ~/.gazebo/models/coman
+
+    echo "Creating srdf from coman.srdf.xacro"
+    cd ../coman_srdf/srdf
+    rosrun xacro xacro.py coman.srdf.xacro > coman.srdf 
+    echo "...created coman.srdf!"
+
+    cd $SCRIPT_ROOT
+    cd ../urdf
+    rm coman_config.urdf.xacro
+     
+    echo "Finish! Enjoy COMAN in GAZEBO!"
+
+fi
