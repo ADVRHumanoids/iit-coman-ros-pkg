6893e02d6316285ce5fafd504c60fb10e007c192
Enrico Mingo <enrico.mingo@iit.it>
2015-06-18 14:23:30 +0200

    Deleted useless files

--- coman_urdf/script/get_model_params.py
+++ coman_urdf/script/get_model_params.py
@@ -11,11 +11,11 @@ if __name__ == '__main__':
   if len(sys.argv) < 3:
     print 'Error: correct usage is get_model_params file_name property_name'
   else:
-    coman_config_file = open(sys.argv[1])
-    soup_coman_config = BeautifulSoup(coman_config_file,"xml")
+    bigman_config_file = open(sys.argv[1])
+    soup_bigman_config = BeautifulSoup(bigman_config_file,"xml")
 
     try:
-      config_el = soup_coman_config.find(attrs={"name": sys.argv[2]})
+      config_el = soup_bigman_config.find(attrs={"name": sys.argv[2]})
       if config_el and config_el.has_attr('value'):
         print config_el['value']
 
