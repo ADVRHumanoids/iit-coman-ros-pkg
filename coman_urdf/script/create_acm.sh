2389fdbe2955575115fbf265cf102a5e9f72b6bf
Enrico Mingo <enrico.mingo@iit.it>
2015-06-24 15:49:18 +0200

    Added & fixed coman model to be compatible with toolbox for collision and capsules.

--- coman_urdf/script/create_acm.sh
+++ coman_urdf/script/create_acm.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
 
-# this way the script can be called from any directory
-SCRIPT_ROOT=$(dirname $(readlink --canonicalize --no-newline $BASH_SOURCE))
-cd $SCRIPT_ROOT
-cd ../urdf
-
 RED='\033[0;31m'
 PURPLE='\033[0;35m'
 GREEN='\033[0;32m'
 ORANGE='\033[0;33m'
 YELLOW='\033[1;33m'
 NC='\033[0m'
+
+if [ "$#" -lt 1 ]; then
+       printf "${RED}No robot name argument passed!${NC}" 
+       echo 
+       echo "Try something like:"
+       echo "./create_urdf_srdf_sdf.sh coman"
+       exit
+fi
+
+robot_name="$1"
+printf "Robot Name is ${GREEN}${robot_name}${NC}"
+echo
+
+# this way the script can be called from any directory
+SCRIPT_ROOT=$(dirname $(readlink --canonicalize --no-newline $BASH_SOURCE))
+cd $SCRIPT_ROOT
+cd ../urdf
 
 if [ -d config ]; then
 
@@ -34,17 +46,17 @@ if [ -d config ]; then
                 echo
                 echo
                 printf "${RED}computing default allowed collision detection matrix for ${model_filename}...${NC}\n"
-                moveit_compute_default_collisions --urdf_path ../urdf/${model_filename}.urdf --srdf_path ../../bigman_srdf/srdf/${model_filename}.srdf --num_trials 100000
-                ./save_acm.py ../../bigman_srdf/srdf/${model_filename}.srdf --output
+                moveit_compute_default_collisions --urdf_path ../urdf/${model_filename}.urdf --srdf_path ../../${robot_name}_srdf/srdf/${model_filename}.srdf --num_trials 100000
+                ./save_acm.py ../../${robot_name}_srdf/srdf/${model_filename}.srdf --output
                 printf "${GREEN}..finished computing default allowed collision detection matrix for ${model_filename}${NC}\n"
 
                 echo
                 echo
                 printf "${RED}computing default allowed collision detection matrix for ${model_filename}_capsules...${NC}\n"
                 #ATM using cylinders - and not capsules - is more conservative when creating the ACM
-                #moveit_compute_default_collisions --urdf_path ../urdf/${model_filename}_capsules.urdf --srdf_path ../../bigman_srdf/srdf/${model_filename}_capsules.srdf --num_trials 75000 --cylinders_to_capsules
-                moveit_compute_default_collisions --urdf_path ../urdf/${model_filename}_capsules.urdf --srdf_path ../../bigman_srdf/srdf/${model_filename}_capsules.srdf --num_trials 75000
-                ./save_acm.py ../../bigman_srdf/srdf/${model_filename}_capsules.srdf --output
+                #moveit_compute_default_collisions --urdf_path ../urdf/${model_filename}_capsules.urdf --srdf_path ../../${robot_name}_srdf/srdf/${model_filename}_capsules.srdf --num_trials 75000 --cylinders_to_capsules
+                moveit_compute_default_collisions --urdf_path ../urdf/${model_filename}_capsules.urdf --srdf_path ../../${robot_name}_srdf/srdf/${model_filename}_capsules.srdf --num_trials 75000
+                ./save_acm.py ../../${robot_name}_srdf/srdf/${model_filename}_capsules.srdf --output
                 printf "${GREEN}..finished computing default allowed collision detection matrix for ${model_filename}_capsule${NC}\n"
             else
                 echo
